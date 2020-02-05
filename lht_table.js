03c162dac5e2fc1d37b9fb89839d1b6d37c9a615
David Lopez <dlopez222@west-mec.org>
2020-02-04 17:28:01 -0700

    finished JS for table

--- lht_table.js
+++ lht_table.js
@@ -12,22 +12,27 @@
 */
 
 var thisDay = new Date("August 30, 2018");
-var tableHTML = "<table id=eventTable><caption>" + "Upcoming Events</caption><tr><th>Date</th><th>" + "Event</th><th>Price</th></tr>";
-var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000);
-var eventDates = thisDay.getDate();
-for (var i = 0; i < eventDates.length; i++)
-{
-    var eventDate = new Date(eventDates[i]);
-    var eventDay = eventDate.toDateString();
-    var eventTime = eventDate.toLocaleTimeString();
-    if (thisDay <= eventDate && eventDate <= endDate)
-    {"<tr><td>eventDay @ eventTime</td> + <td>description</td> + <td>price</td></tr>";}
+var tableHTML = "<table id='eventTable'>";
+tableHTML += "<caption>Upcoming Events</caption>";
+tableHTML += "<tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+/* Set the end date for the event list two weeks */
+var endDate = new Date(thisDay.getTime() + 14*24*60*60*1000);
+/* Loop through eventDates array */
+for (var i = 0; i < eventDates.length; i++) {
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = eventDate.toDateString();
+   var eventTime = eventDate.toLocaleTimeString();
+   /* If the event date is within the two-week window, display it on the page */
+   if (thisDay <= eventDate && eventDate <= endDate) {
+      tableHTML += "<tr>";
+      tableHTML += "<td>" + eventDay + " @ " + eventTime + "</td>";
+      tableHTML += "<td>" + eventDescriptions[i] + "</td>";
+      tableHTML += "<td>" + eventPrices[i] + "</td>";
+      tableHTML += "</tr>";
+   }
 }
-    
+tableHTML += "</table>";
 document.getElementById("eventList").innerHTML = tableHTML;
-
-
-
 
 
 
